ad8e49d4d900c754da89dca9ab480b89b2dc2ab4
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-11-02 15:09:21 -0400

    working gym env

--- gymEnv.py
+++ gymEnv.py
@@ -1,3 +1,5 @@
+import math
+
 import gym
 import melee
 from gym import spaces
@@ -7,26 +9,38 @@ from stable_baselines3.common.env_checker import check_env
 import numpy as np
 import game
 
+num_actions = 3  # walk left/right, jump, drop, attack
+
 
 class Observation:
     player_x: int
     player_y: int
     opponent_x: int
     opponent_y: int
+    action_frame: int
 
     def getValues(self):
-        return np.array([self.player_x, self.player_y, self.opponent_x, self.opponent_y])
+        return np.array([self.player_x, self.player_y, self.opponent_x, self.opponent_y, self.action_frame])
 
 
-class CharacterEnv:
-    def __init__(self, state: melee.GameState, port, port_opponent):
+class CharacterEnv(gym.Env):
+    def __init__(self, game, state: melee.GameState, port, port_opponent):
+
+        self.game = game
         super(CharacterEnv, self).__init__()
+        self.action_frame = 1
+        self.last_dist = 0
         self.port = port
         self.port_opponent = port_opponent
+
         self.gamestate = state
 
         base_observation = self.getObservation()
-        self.observation_space = spaces.Box(shape=base_observation.shape[0], high=1, low=-1)
+
+        self.observation_space = spaces.Box(shape=base_observation.shape, high=1, low=-1)
+        self.action_space = spaces.Discrete(num_actions)
+
+        self.controller: melee.Controller = game.getController(port)
 
     def setGameState(self, state: melee.GameState):
         self.gamestate = state
@@ -45,6 +59,58 @@ class CharacterEnv:
         ob.player_y = player_data.y / stage_edge_y
         ob.opponent_x = opponent_data.x / stage_edge_x
         ob.opponent_y = opponent_data.y / stage_edge_y
-
-        print(ob.player_x)
+        ob.action_frame = self.action_frame
         return ob.getValues()
+
+    def step(self, action: int):
+        self.action_frame *= -1
+
+        if action == 0:  # Walk Left
+            self.move(-1, 0)
+        elif action == 1:  # Walk Right
+            self.move(1, 0)
+        # elif action == 2:  # Jump
+        #     self.jump()
+        # elif action == 3:  # Drop
+        #     self.move(0, -1)
+        else:  # Attack
+            self.move(0, 0)
+        self.flush_controls()
+
+        self.gamestate: melee.GameState = self.game.getState()
+
+        player: melee.PlayerState = self.gamestate.players.get(self.port)
+        opponent: melee.PlayerState = self.gamestate.players.get(self.port_opponent)
+
+        r = self.reward(player, opponent)
+        return [self.getObservation(), r, False, {}]
+
+    def reward(self, player: melee.PlayerState, opponent: melee.PlayerState):
+        dist = math.dist((player.x, player.y), (opponent.x, opponent.y))
+
+        r = (self.last_dist - dist) * (10-dist)
+        print(r)
+        return r
+
+    def reset(self):
+        pass
+
+    ################
+    ### Controls ###
+    ################
+    def flush_controls(self):
+        if (self.action_frame == -1):
+            self.controller.release_button(melee.Button.BUTTON_Y)
+            self.controller.release_button(melee.Button.BUTTON_A)
+        self.controller.flush()
+
+    def move(self, x: float, y: float):
+        self.controller.tilt_analog_unit(melee.Button.BUTTON_MAIN, x, y)
+
+    def jump(self):
+        self.controller.press_button(melee.Button.BUTTON_Y)
+        self.controller.flush()
+
+    def jab(self):
+        self.controller.press_button(melee.Button.BUTTON_A)
+        self.controller.flush()
--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import argparse
 import math
 
 import game
-import random
 import melee
-
-# This example program demonstrates how to use the Melee API to run a console,
-#   setup controllers, and send button presses over to a console
 
 def check_port(value):
     ivalue = int(value)
@@ -42,54 +38,8 @@ args: game.Args = parser.parse_args()
 
 attacking = 0
 game = game.Game(args=args)
-controller = game.getController()
-controller_opponent = game.getControllerOpponent()
+controller = game.getController(args.port)
+controller_opponent = game.getController(args.opponent)
 
-game.enterMatch()
-jumping = False
-moveY = 0
-while True:
-    gamestate = game.getState()
+game.enterMatch(stage=melee.Stage.FINAL_DESTINATION)
 
-    # Slippi Online matches assign you a random port once you're in game that's different
-    #   than the one you're physically plugged into. This helper will autodiscover what
-    #   port we actually are.
-    discovered_port = args.port
-    if args.connect_code != "":
-        discovered_port = melee.gamestate.port_detector(gamestate, melee.Character.FOX, 0)
-    if discovered_port > 0:
-        player_state: melee.PlayerState = gamestate.players.get(discovered_port)
-        oppenent_state: melee.PlayerState = gamestate.players.get(1)
-
-
-
-        print(player_state.action)
-        if(oppenent_state.y > player_state.y and not jumping):
-            controller.press_button(melee.Button.BUTTON_Y)
-            jumping = True
-            moveY = 0
-
-        elif(oppenent_state.y<player_state.y and player_state.on_ground and moveY == 0):
-            moveY = -1
-
-        else:
-            controller.release_button(melee.Button.BUTTON_Y)
-            jumping = False
-            moveY = 0
-
-
-        if(player_state.x < oppenent_state.x and oppenent_state.on_ground):
-            controller.tilt_analog_unit(melee.Button.BUTTON_MAIN, 1, moveY)
-        elif (player_state.x > oppenent_state.x and oppenent_state.on_ground):
-            controller.tilt_analog_unit(melee.Button.BUTTON_MAIN, -1, moveY)
-        else:
-            controller.tilt_analog(melee.Button.BUTTON_MAIN, 0, 0)
-
-        if(attacking >= 10 and moveY == 0 and math.dist((player_state.x, player_state.y), (oppenent_state.x, oppenent_state.y)) < 5):
-            controller.press_button(melee.Button.BUTTON_A)
-            attacking = 0
-        else:
-            controller.release_button(melee.Button.BUTTON_A)
-            attacking += 1
-
-        controller.flush()
