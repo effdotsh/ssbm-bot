ce8cd1e627e9fde6532d1eed2eae9af3f37d8694
effdotsh <ephram@eff.sh>
2021-11-03 21:14:26 -0400

    pytorch sequential

--- network.py
+++ network.py
@@ -1,43 +1,49 @@
 import os
 import torch
 from torch import nn
-import random
 import matplotlib.pyplot as plt
+import numpy as np
 
 
+import random
+np.array([])
 class CharacterAI(nn.Module):
-    def __init__(self, num_inputs, num_outputs):
+    def __init__(self, architecture):
         super().__init__()
-        self.network = nn.Linear(num_inputs, num_outputs)
+        self.layers = nn.Sequential(
+            nn.Linear(4, 2),
+            # nn.ReLU(),
+            nn.Linear(2, 1),
+        )
     def forward(self, inputs):
-        return self.network(inputs)
+        return self.layers(inputs)
 
 def rand_value():
     return random.random() - 0.5 * 100
 if __name__== '__main__':
     torch.manual_seed(1)
-    ai = CharacterAI(3, 1)
-
-    x = torch.tensor([[1.0, 2.0, 1.0], [1.0, 2.0, 5.0]])
+    ai = CharacterAI([3, 1])
+    x = torch.tensor([[1.0, 2.0, 1.0, 6.0], [0.4, 1.0, 2.0, 5.0]])
     print(ai.forward(x))
 
-    [w, b] = ai.parameters()
-    w0 = w[0][0].item()
-    b0 = b[0].item()
-    print(w0, b0)
+    # params = ai.parameters()
+    # for p in params:
+        # print(p)
+
 
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(ai.parameters(), lr=0.001)
 
-    epochs = 50000
+    epochs = 15000
 
     losses = []
     for i in range(epochs):
         num1 = rand_value()
         num2 = rand_value()
         num3 = rand_value()
-        input = [num1, num2, num3]
-        output = torch.tensor([num1 * num2 / num3 + 1000])
+        num4 = rand_value()
+        input = [num1, num2, num3, num4]
+        output = torch.tensor([num1 * num2 / num3 + num4*1000])
         pred = ai.forward(torch.tensor(input))
         loss = criterion(pred, output)
 
