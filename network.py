c6a1c89ae7a191790a8cab5f7186bb7dfee6c504
effdotsh <ephram@eff.sh>
2021-11-03 23:01:55 -0400

    coppied some code over from https://keras.io/examples/rl/actor_critic_cartpole/

--- network.py
+++ network.py
@@ -1,6 +1,8 @@
-import os
-import torch
-from torch import nn
+from numpy import loadtxt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,52 +10,123 @@ import numpy as np
 import random
 np.array([])
 class CharacterAI(nn.Module):
-    def __init__(self, architecture):
+    def __init__(self, learning_rate=0.01):
         super().__init__()
-        self.layers = nn.Sequential(
-            nn.Linear(4, 2),
-            # nn.ReLU(),
-            nn.Linear(2, 1),
-        )
+        # self.model = Sequential()
+        # self.model.add(Dense(2, input_dim=4, activation="sigmoid"))
+        # self.model.add(Dense(1, activation="sigmoid"))
+
+        inputs = layers.Input(shape=(4,))
+        common = layers.Dense(2, activation="sigmoid")(inputs)
+        action = layers.Dense(1, activation="softmax")(common)
+        critic = layers.Dense(1)(common)
+
+        self.model = keras.Model(inputs=inputs, outputs=[action, critic])
+
     def forward(self, inputs):
         return self.layers(inputs)
 
 def rand_value():
     return random.random() - 0.5 * 100
 if __name__== '__main__':
-    torch.manual_seed(1)
-    ai = CharacterAI([3, 1])
-    x = torch.tensor([[1.0, 2.0, 1.0, 6.0], [0.4, 1.0, 2.0, 5.0]])
-    print(ai.forward(x))
 
     # params = ai.parameters()
     # for p in params:
         # print(p)
 
-
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(ai.parameters(), lr=0.001)
-
     epochs = 15000
 
-    losses = []
+    optimizer = keras.optimizers.Adam(learning_rate=0.01)
+    huber_loss = keras.losses.Huber()
+    action_probs_history = []
+    critic_value_history = []
+    rewards_history = []
+    running_reward = 0
+    episode_count = 0
     for i in range(epochs):
         num1 = rand_value()
         num2 = rand_value()
         num3 = rand_value()
         num4 = rand_value()
         input = [num1, num2, num3, num4]
-        output = torch.tensor([num1 * num2 / num3 + num4*1000])
-        pred = ai.forward(torch.tensor(input))
-        loss = criterion(pred, output)
+        output = tf.tensor([num1 * num2 / num3 + num4*1000])
 
-        print(f'epoch: {i}, loss: {loss}')
-        losses.append(loss.item())
+        episode_reward = 0
+        with tf.GradientTape() as tape:
+            for timestep in range(1, max_steps_per_episode):
+                # env.render(); Adding this line would show the attempts
+                # of the agent in a pop up window.
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+                state = tf.convert_to_tensor(state)
+                state = tf.expand_dims(state, 0)
 
-    print(losses[0])
-    plt.plot(range(epochs),losses)
+                # Predict action probabilities and estimated future rewards
+                # from environment state
+                action_probs, critic_value = model(state)
+                critic_value_history.append(critic_value[0, 0])
+
+                # Sample action from action probability distribution
+                action = np.random.choice(num_actions, p=np.squeeze(action_probs))
+                action_probs_history.append(tf.math.log(action_probs[0, action]))
+
+                # Apply the sampled action in our environment
+                state, reward, done, _ = env.step(action)
+                rewards_history.append(reward)
+                episode_reward += reward
+
+                if done:
+                    break
+
+            # Update running reward to check condition for solving
+            running_reward = 0.05 * episode_reward + (1 - 0.05) * running_reward
+
+            # Calculate expected value from rewards
+            # - At each timestep what was the total reward received after that timestep
+            # - Rewards in the past are discounted by multiplying them with gamma
+            # - These are the labels for our critic
+            returns = []
+            discounted_sum = 0
+            for r in rewards_history[::-1]:
+                discounted_sum = r + gamma * discounted_sum
+                returns.insert(0, discounted_sum)
+
+            # Normalize
+            returns = np.array(returns)
+            returns = (returns - np.mean(returns)) / (np.std(returns) + eps)
+            returns = returns.tolist()
+
+            # Calculating loss values to update our network
+            history = zip(action_probs_history, critic_value_history, returns)
+            actor_losses = []
+            critic_losses = []
+            for log_prob, value, ret in history:
+                # At this point in history, the critic estimated that we would get a
+                # total reward = `value` in the future. We took an action with log probability
+                # of `log_prob` and ended up recieving a total reward = `ret`.
+                # The actor must be updated so that it predicts an action that leads to
+                # high rewards (compared to critic's estimate) with high probability.
+                diff = ret - value
+                actor_losses.append(-log_prob * diff)  # actor loss
+
+                # The critic must be updated so that it predicts a better estimate of
+                # the future rewards.
+                critic_losses.append(
+                    huber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
+                )
+
+            # Backpropagation
+            loss_value = sum(actor_losses) + sum(critic_losses)
+            grads = tape.gradient(loss_value, model.trainable_variables)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+            # Clear the loss and reward history
+            action_probs_history.clear()
+            critic_value_history.clear()
+            rewards_history.clear()
+
+
+
+
+    print(rewards_history[0])
+    plt.plot(range(epochs),rewards_history)
     plt.show()
