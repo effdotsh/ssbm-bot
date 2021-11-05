833c3b29bffa7cca2e6e8df3453da718e774df89
effdotsh <ephram@eff.sh>
2021-11-05 18:15:08 -0400

    Moved ppo stuff to its own class

--- network.py
+++ network.py
@@ -1,132 +1,50 @@
-from numpy import loadtxt
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-
 import random
-np.array([])
-class CharacterAI(nn.Module):
-    def __init__(self, learning_rate=0.01):
-        super().__init__()
-        # self.model = Sequential()
-        # self.model.add(Dense(2, input_dim=4, activation="sigmoid"))
-        # self.model.add(Dense(1, activation="sigmoid"))
-
-        inputs = layers.Input(shape=(4,))
-        common = layers.Dense(2, activation="sigmoid")(inputs)
-        action = layers.Dense(1, activation="softmax")(common)
-        critic = layers.Dense(1)(common)
-
-        self.model = keras.Model(inputs=inputs, outputs=[action, critic])
-
-    def forward(self, inputs):
-        return self.layers(inputs)
-
+from PPO import PPO
 def rand_value():
     return random.random() - 0.5 * 100
+
+
 if __name__== '__main__':
+    action_dim = [1]
+    state_dim = [4]
+
+
+
+    max_ep_len = 400  # max timesteps in one episode
+    max_training_timesteps = int(1e5)  # break training loop if timeteps > max_training_timesteps
+
+    print_freq = max_ep_len * 4  # print avg reward in the interval (in num timesteps)
+    log_freq = max_ep_len * 2  # log avg reward in the interval (in num timesteps)
+    save_model_freq = int(2e4)  # save model frequency (in num timesteps)
+
+    action_std = None
+
+    #####################################################
+
+    ## Note : print/log frequencies should be > than max_ep_len
+
+    ################ PPO hyperparameters ################
+
+    update_timestep = max_ep_len * 4  # update policy every n timesteps
+    K_epochs = 40  # update policy for K epochs
+    eps_clip = 0.2  # clip parameter for PPO
+    gamma = 0.99  # discount factor
+
+    lr_actor = 0.0003  # learning rate for actor network
+    lr_critic = 0.001  # learning rate for critic network
+    has_continuous_action_space = True
+
+    random_seed = 0  # set random seed if required (0 = no random seed)
+
+
+    ppo_agent = PPO(state_dim, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip, has_continuous_action_space,
+                    action_std)
 
     # params = ai.parameters()
     # for p in params:
         # print(p)
 
-    epochs = 15000
-
-    optimizer = keras.optimizers.Adam(learning_rate=0.01)
-    huber_loss = keras.losses.Huber()
-    action_probs_history = []
-    critic_value_history = []
-    rewards_history = []
-    running_reward = 0
-    episode_count = 0
-    for i in range(epochs):
-        num1 = rand_value()
-        num2 = rand_value()
-        num3 = rand_value()
-        num4 = rand_value()
-        input = [num1, num2, num3, num4]
-        output = tf.tensor([num1 * num2 / num3 + num4*1000])
-
-        episode_reward = 0
-        with tf.GradientTape() as tape:
-            for timestep in range(1, max_steps_per_episode):
-                # env.render(); Adding this line would show the attempts
-                # of the agent in a pop up window.
-
-                state = tf.convert_to_tensor(state)
-                state = tf.expand_dims(state, 0)
-
-                # Predict action probabilities and estimated future rewards
-                # from environment state
-                action_probs, critic_value = model(state)
-                critic_value_history.append(critic_value[0, 0])
-
-                # Sample action from action probability distribution
-                action = np.random.choice(num_actions, p=np.squeeze(action_probs))
-                action_probs_history.append(tf.math.log(action_probs[0, action]))
-
-                # Apply the sampled action in our environment
-                state, reward, done, _ = env.step(action)
-                rewards_history.append(reward)
-                episode_reward += reward
-
-                if done:
-                    break
-
-            # Update running reward to check condition for solving
-            running_reward = 0.05 * episode_reward + (1 - 0.05) * running_reward
-
-            # Calculate expected value from rewards
-            # - At each timestep what was the total reward received after that timestep
-            # - Rewards in the past are discounted by multiplying them with gamma
-            # - These are the labels for our critic
-            returns = []
-            discounted_sum = 0
-            for r in rewards_history[::-1]:
-                discounted_sum = r + gamma * discounted_sum
-                returns.insert(0, discounted_sum)
-
-            # Normalize
-            returns = np.array(returns)
-            returns = (returns - np.mean(returns)) / (np.std(returns) + eps)
-            returns = returns.tolist()
-
-            # Calculating loss values to update our network
-            history = zip(action_probs_history, critic_value_history, returns)
-            actor_losses = []
-            critic_losses = []
-            for log_prob, value, ret in history:
-                # At this point in history, the critic estimated that we would get a
-                # total reward = `value` in the future. We took an action with log probability
-                # of `log_prob` and ended up recieving a total reward = `ret`.
-                # The actor must be updated so that it predicts an action that leads to
-                # high rewards (compared to critic's estimate) with high probability.
-                diff = ret - value
-                actor_losses.append(-log_prob * diff)  # actor loss
-
-                # The critic must be updated so that it predicts a better estimate of
-                # the future rewards.
-                critic_losses.append(
-                    huber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
-                )
-
-            # Backpropagation
-            loss_value = sum(actor_losses) + sum(critic_losses)
-            grads = tape.gradient(loss_value, model.trainable_variables)
-            optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
-            # Clear the loss and reward history
-            action_probs_history.clear()
-            critic_value_history.clear()
-            rewards_history.clear()
 
 
-
-
-    print(rewards_history[0])
-    plt.plot(range(epochs),rewards_history)
-    plt.show()
+    # plt.plot(range(epochs),rewards_history)
+    # plt.show()
