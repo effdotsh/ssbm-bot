174b9d6072f95b24566d74aed8b38b9c180438fe
effdotsh <ephram@eff.sh>
2021-11-06 01:18:37 -0400

    PPo demo

--- network.py
+++ network.py
@@ -1,50 +1,17 @@
-import random
-from PPO import PPO
-def rand_value():
-    return random.random() - 0.5 * 100
+import gym
+from stable_baselines3 import PPO
+
+# Parallel environments
+import gymEnv
+
+import matplotlib.pyplot as plt
 
 
-if __name__== '__main__':
-    action_dim = [1]
-    state_dim = [4]
+env = gymEnv.CharacterEnv()
+# env = make_vec_env("CartPole-v1", n_envs=4)
 
+model = PPO("MlpPolicy", env, verbose=1, learning_rate=0.005)
+model.learn(total_timesteps=50000)
 
-
-    max_ep_len = 400  # max timesteps in one episode
-    max_training_timesteps = int(1e5)  # break training loop if timeteps > max_training_timesteps
-
-    print_freq = max_ep_len * 4  # print avg reward in the interval (in num timesteps)
-    log_freq = max_ep_len * 2  # log avg reward in the interval (in num timesteps)
-    save_model_freq = int(2e4)  # save model frequency (in num timesteps)
-
-    action_std = None
-
-    #####################################################
-
-    ## Note : print/log frequencies should be > than max_ep_len
-
-    ################ PPO hyperparameters ################
-
-    update_timestep = max_ep_len * 4  # update policy every n timesteps
-    K_epochs = 40  # update policy for K epochs
-    eps_clip = 0.2  # clip parameter for PPO
-    gamma = 0.99  # discount factor
-
-    lr_actor = 0.0003  # learning rate for actor network
-    lr_critic = 0.001  # learning rate for critic network
-    has_continuous_action_space = True
-
-    random_seed = 0  # set random seed if required (0 = no random seed)
-
-
-    ppo_agent = PPO(state_dim, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip, has_continuous_action_space,
-                    action_std)
-
-    # params = ai.parameters()
-    # for p in params:
-        # print(p)
-
-
-
-    # plt.plot(range(epochs),rewards_history)
-    # plt.show()
+plt.plot(env.rewards)
+plt.show()
