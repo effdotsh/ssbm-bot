c23ba8e285a7de37674c715a6894b8286e5b13e9
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-11-06 01:18:37 -0400

    PPo demo

--- gymEnv.py
+++ gymEnv.py
@@ -1,116 +1,45 @@
-import math
-
 import gym
-import melee
+import random
 from gym import spaces
-from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.env_checker import check_env
-
+import math
 import numpy as np
-import game
 
-num_actions = 3  # walk left/right, jump, drop, attack
+frames_per_step = 100
 
-
-class Observation:
-    player_x: int
-    player_y: int
-    opponent_x: int
-    opponent_y: int
-    action_frame: int
-
-    def getValues(self):
-        return np.array([self.player_x, self.player_y, self.opponent_x, self.opponent_y, self.action_frame])
-
-
+def rand_value():
+    return random.random()-0.5 * 1000
+def getObs():
+    return np.array([rand_value(), rand_value(), rand_value()])
 class CharacterEnv(gym.Env):
-    def __init__(self, game, state: melee.GameState, port, port_opponent):
-
-        self.game = game
+    def __init__(self):
         super(CharacterEnv, self).__init__()
-        self.action_frame = 1
-        self.last_dist = 0
-        self.port = port
-        self.port_opponent = port_opponent
 
-        self.gamestate = state
+        self.obs = getObs()
+        self.r = 0
+        self.frame = 0
 
-        base_observation = self.getObservation()
+        self.observation_space = spaces.Box(shape=np.array([3]), dtype=np.float, low=-500, high=500)
+        self.action_space = spaces.Box(shape=np.array([1]), dtype=np.float, low = -1, high=1)
 
-        self.observation_space = spaces.Box(shape=base_observation.shape, high=1, low=-1)
-        self.action_space = spaces.Discrete(num_actions)
+        self.rewards = []
+    def step(self, action: float):
+        action *= 1500
+        self.frame += 1
+        self.frame %= frames_per_step
 
-        self.controller: melee.Controller = game.getController(port)
+        if(self.frame == 0):
+            print(self.r)
+            self.rewards.append(self.r)
+            self.r = 0
 
-    def setGameState(self, state: melee.GameState):
-        self.gamestate = state
+        actual = self.obs[0] + self.obs[1] + self.obs[2]
+        r = -math.fabs(action - actual)
+        self.r += r
+        self.obs = getObs()
+        return [self.obs, r if self.frame == frames_per_step-1 else 0, self.frame == 0, {}]
 
-    def getObservation(self):
-        gamestate = self.gamestate
-        player_data: melee.PlayerState = gamestate.players.get(self.port)
-        opponent_data: melee.PlayerState = gamestate.players.get(self.port_opponent)
 
-        stage_blastzones: melee.Stage = melee.stages.BLASTZONES[gamestate.stage]
-        stage_edge_x = stage_blastzones[1]
-        stage_edge_y = stage_blastzones[3]
-
-        ob = Observation()
-        ob.player_x = player_data.x / stage_edge_x
-        ob.player_y = player_data.y / stage_edge_y
-        ob.opponent_x = opponent_data.x / stage_edge_x
-        ob.opponent_y = opponent_data.y / stage_edge_y
-        ob.action_frame = self.action_frame
-        return ob.getValues()
-
-    def step(self, action: int):
-        self.action_frame *= -1
-
-        if action == 0:  # Walk Left
-            self.move(-1, 0)
-        elif action == 1:  # Walk Right
-            self.move(1, 0)
-        # elif action == 2:  # Jump
-        #     self.jump()
-        # elif action == 3:  # Drop
-        #     self.move(0, -1)
-        else:  # Attack
-            self.move(0, 0)
-        self.flush_controls()
-
-        self.gamestate: melee.GameState = self.game.getState()
-
-        player: melee.PlayerState = self.gamestate.players.get(self.port)
-        opponent: melee.PlayerState = self.gamestate.players.get(self.port_opponent)
-
-        r = self.reward(player, opponent)
-        return [self.getObservation(), r, False, {}]
-
-    def reward(self, player: melee.PlayerState, opponent: melee.PlayerState):
-        dist = math.dist((player.x, player.y), (opponent.x, opponent.y))
-
-        r = (self.last_dist - dist) * (10-dist)
-        print(r)
-        return r
 
     def reset(self):
-        pass
+        return self.obs
 
-    ################
-    ### Controls ###
-    ################
-    def flush_controls(self):
-        if (self.action_frame == -1):
-            self.controller.release_button(melee.Button.BUTTON_Y)
-            self.controller.release_button(melee.Button.BUTTON_A)
-        self.controller.flush()
-
-    def move(self, x: float, y: float):
-        self.controller.tilt_analog_unit(melee.Button.BUTTON_MAIN, x, y)
-
-    def jump(self):
-        self.controller.press_button(melee.Button.BUTTON_Y)
-        self.controller.flush()
-
-    def jab(self):
-        self.controller.press_button(melee.Button.BUTTON_A)
-        self.controller.flush()
