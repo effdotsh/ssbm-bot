7978e1bf70cf44b6a718f276894e416145a2fc78
effdotsh <ephram@eff.sh>
2021-12-25 22:26:40 -0500

    started dqn torch port

---
+++ EasyML/DQNTorch.py
@@ -0,0 +1,136 @@
+import copy
+import random
+
+import torch
+from torch import nn
+
+from torchsample.modules import ModuleTrainer
+import time
+
+from collections import deque
+
+import numpy as np
+
+import datetime
+
+
+class DQNetwork(nn.Module):
+    def __init__(self, num_inputs: int, num_outputs: int):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(num_inputs, 32),
+            nn.Tanh(),
+            nn.Linear(32, 32),
+            nn.Tanh(),
+            nn.Linear(32, 32),
+            nn.Tanh(),
+            nn.Linear(32, 32),
+            nn.Tanh(),
+            nn.Linear(32, num_outputs)
+        )
+
+    def forward(self, inputs):
+        return self.layers(inputs)
+
+
+class DQNAgent:
+    def __init__(self, num_inputs, num_outputs, learning_rate=0.001, min_replay_size=10_000, max_replay_size=50_000,
+                 minibatch_size=128, discount_factor=0.9, update_target_every=5, epsilon=1, min_epsilon=0.01,
+                 epsilon_decay=0.99):
+        # Gets Trained
+        self.model = self.create_model(num_inputs=num_inputs, num_outputs=num_outputs)
+
+        # Gets predicted from
+        # TODO: use the pytorch equivilent of keras' get_weights and set_weights. This implementation is kinda sloppy
+        self.target_model = copy.deepcopy(self.model)
+
+        self.trainer = ModuleTrainer(self.model)
+        self.trainer.compile(loss='mse_loss',
+                             optimizer='adam')
+        optim = torch.optim.Adam(self.target_model.parameters(),
+                                 lr=learning_rate)
+        self.trainer.set_optimizer(optim)
+
+        self.min_replay_size = min_replay_size
+        self.replay_memory = deque(maxlen=max_replay_size)
+
+        self.target_update_counter = 0
+        self.minibatch_size = minibatch_size
+        self.discount_factor = discount_factor
+        self.update_target_every = update_target_every
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+
+        self.epsilon_decay = epsilon_decay
+        self.min_epsilon = min_epsilon
+        self.epsilon = epsilon
+
+    def create_model(self, num_inputs: int, num_outputs: int):
+        model = DQNetwork(num_inputs, num_outputs)
+
+        return model
+
+    def update_replay_memory(self, transition):
+        self.replay_memory.append(transition)
+
+    def get_qs(self, state):
+        return self.model.forward(torch.Tensor(state))
+
+    def predict(self, state):
+        if np.random.random() < self.epsilon:
+            # Get action from Q table
+            action = np.argmax(self.get_qs(state).detach().numpy())
+        else:
+            # Get random action
+            action = np.random.randint(0, self.num_outputs)
+
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.min_epsilon, self.epsilon)
+
+        return action
+
+    def train(self, terminal_state: bool):
+        if len(self.replay_memory) < self.min_replay_size:
+            return
+
+        minibatch = random.sample(self.replay_memory, self.minibatch_size)
+
+        # for transition in minibatch:
+        #     print(torch.Tensor(transition[0]))
+
+        current_states = torch.Tensor([np.array(transition[0]) for transition in minibatch])
+
+        # print(current_states)
+        current_qs_list = self.model.forward(current_states)
+
+        new_current_states = np.array([transition[3] for transition in minibatch])
+
+        future_qs_list = self.target_model.forward(torch.Tensor(new_current_states))
+
+        X = []
+        y = []
+
+        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+            if not done:
+                max_future_q = np.max(future_qs_list[index].detach().numpy())
+                new_q = reward + self.discount_factor * max_future_q
+            else:
+                new_q = reward
+
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            X.append(current_state)
+            y.append(current_qs.detach().numpy())
+
+        # self.model.fit(np.array(X), np.array(y), batch_size=self.minibatch_size, verbose=0, shuffle=False,
+        #                callbacks=None, use_multiprocessing=True)
+        print(torch.Tensor(y))
+        self.trainer.fit(torch.Tensor(X), torch.Tensor(y), batch_size=self.minibatch_size, verbose=0)
+
+        if terminal_state:
+            self.target_update_counter += 1
+
+        if self.target_update_counter > self.update_target_every:
+            #TODO: Same as above
+            self.target_model = copy.deepcopy(self.model)
