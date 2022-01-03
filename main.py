40753453a3d1898e265fc7cb7e29b3c73a793aad
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2022-01-02 19:52:20 -0500

    Blocking inputs

--- main.py
+++ main.py
@@ -58,20 +58,20 @@ args: gameManager.Args = parser.parse_args()
 
 start_time = time.time()
 if __name__ == '__main__':
-    character = melee.Character.JIGGLYPUFF
+    character = melee.Character.FOX
 
     if not os.path.isdir(f'{args.model_path}/{character.name}'):
         os.makedirs(f'{args.model_path}/{character.name}')
 
     game = gameManager.Game(args)
-    game.enterMatch(cpu_level=9 if args.load_from < 0 else 0, opponant_character=character, player_character=character)
+    game.enterMatch(cpu_level=7 if args.load_from < 0 else 0, opponant_character=character, player_character=character)
     step = args.load_from
 
     if args.load_from < 0:  # Start training against CPU
         agent1 = CharacterController(port=args.port, opponent_port=args.opponent, game=game,
-                                     moveset=CharacterMovesets[character.name].value, min_replay_size=2000, minibatch_size=128,
+                                     moveset=CharacterMovesets[character.name].value, min_replay_size=5000, minibatch_size=128,
                                      max_replay_size=300_000,
-                                     learning_rate=0.00004, update_target_every=2, discount_factor=0.999,
+                                     learning_rate=0.00004, update_target_every=10, discount_factor=0.999,
                                      epsilon_decay=0.9997, epsilon=1)
 
     else:  # Use TLto self-train
