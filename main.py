6cd2562e484a4acb4b85fa6ac31aac8572df4507
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2022-03-05 21:57:01 -0500

    Compete

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ args = Args.get_args()
 start_time = time.time()
 if __name__ == '__main__':
     character = melee.Character.MARTH
-    opponent = melee.Character.JIGGLYPUFF if not args.compete else character
+    opponent = melee.Character.CPTFALCON
 
     if not os.path.isdir(f'{args.model_path}/{character.name}'):
         os.makedirs(f'{args.model_path}/{character.name}')
@@ -32,8 +32,13 @@ if __name__ == '__main__':
     agent1 = Agent(player_port=args.port, opponent_port=args.opponent, game=game, algorithm=Algorithm.PPO,
                    use_wandb=args.wandb)
 
+    if args.compete:
+        agent2 = Agent(player_port=args.opponent, opponent_port=args.port, game=game, algorithm=Algorithm.PPO,
+                       use_wandb=False, log=False)
+
     while True:  # Training loop
         gamestate = game.get_gamestate()
         agent1.run_frame(gamestate)
-
+        if args.compete:
+            agent2.run_frame(gamestate)
         step += 1
