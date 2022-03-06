60c4ebdc716ab26f817294067679402b2769454d
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2022-03-06 14:51:35 -0500

    updated hyperparams

--- ModelCreator.py
+++ ModelCreator.py
@@ -17,7 +17,7 @@ def create_model(algorithm: Algorithm, num_inputs: int, num_actions: int):
     elif algorithm == Algorithm.DQN:
         model = DQN(obs_dim=num_inputs, action_dim=num_actions)
     elif algorithm == Algorithm.PPO:
-        model = PPO(obs_dim=num_inputs, action_dim=num_actions, batch_size=512, T_horizon=1024, learning_rate=3e-5,
+        model = PPO(obs_dim=num_inputs, action_dim=num_actions, batch_size=100, T_horizon=300, learning_rate=3e-5,
                     adv_normalization=True)
     else:
         model = DQN(obs_dim=num_inputs, action_dim=num_actions)
