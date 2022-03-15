3f055955b9b97018cb5597ebd2fe1fe639f3bbd7
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2022-03-14 20:48:12 -0400

    Added qt smash runner

--- ModelCreator.py
+++ ModelCreator.py
@@ -3,12 +3,14 @@ from enum import Enum
 from EasyRL.Discrete.DQN.DQN_Inps import DQN
 from EasyRL.Discrete.PPO.PPO import PPO
 from EasyRL.Discrete.SAC.SAC1.SAC import SAC
+from EasyRL.Discrete.QT.QT import QT
 
 
 class Algorithm(Enum):
     SAC = 0
     DQN = 1
     PPO = 2
+    QT=3
 
 
 def create_model(algorithm: Algorithm, num_inputs: int, num_actions: int):
@@ -19,6 +21,8 @@ def create_model(algorithm: Algorithm, num_inputs: int, num_actions: int):
     elif algorithm == Algorithm.PPO:
         model = PPO(obs_dim=num_inputs, action_dim=num_actions, batch_size=1000, T_horizon=1200, learning_rate=3e-5,
                     adv_normalization=False)
+    elif algorithm == Algorithm.QT:
+        model = QT(obs_dim=num_inputs, action_dim=num_actions, learning_rate=3e-5)
     else:
         model = DQN(obs_dim=num_inputs, action_dim=num_actions)
 
