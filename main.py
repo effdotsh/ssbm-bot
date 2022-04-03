254ac7dc6143daef9b06efcd7504b23b469c3875
effdotsh <ephram@eff.sh>
2022-04-03 10:13:25 -0400

    removed old files

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import platform
 
 import os
 
-from Agent import Agent, Algorithm
 
 args = Args.get_args()
 start_time = time.time()
@@ -20,8 +19,8 @@ if __name__ == '__main__':
     character = melee.Character.CPTFALCON
     opponent = melee.Character.MARTH
 
-    if not os.path.isdir(f'{args.model_path}/{character.name}'):
-        os.makedirs(f'{args.model_path}/{character.name}')
+    # if not os.path.isdir(f'{args.model_path}/{character.name}'):
+    #     os.makedirs(f'{args.model_path}/{character.name}')
 
     game = GameManager.Game(args)
     game.enterMatch(cpu_level=args.cpu_level if not args.compete else 0, opponant_character=opponent,
@@ -29,16 +28,11 @@ if __name__ == '__main__':
                     stage=melee.Stage.FINAL_DESTINATION)
     step = args.load_from
 
-    agent1 = Agent(player_port=args.port, opponent_port=args.opponent, game=game, algorithm=Algorithm.QT,
-                   use_wandb=args.wandb)
 
-    if args.compete:
-        agent2 = Agent(player_port=args.opponent, opponent_port=args.port, game=game, algorithm=Algorithm.QT,
-                       use_wandb=False, log=False)
 
     while True:  # Training loop
         gamestate = game.get_gamestate()
-        agent1.run_frame(gamestate)
-        if args.compete:
-            agent2.run_frame(gamestate)
-        step += 1
+        # agent1.run_frame(gamestate)
+        # if args.compete:
+        #     agent2.run_frame(gamestate)
+        # step += 1
