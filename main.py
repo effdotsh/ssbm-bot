617a034ecc5146a0df4168bd57aca5382cdb406c
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2022-04-09 20:42:10 -0400

    generate output array

--- main.py
+++ main.py
@@ -6,18 +6,15 @@ import melee
 import os
 
 from ReplayManager import filter_replays
+from network import train
 
 args = Args.get_args()
 
-
 MIN_BUFFER_SIZE = 60_000
-
-
 
 if __name__ == '__main__':
     player_character = melee.Character.FALCO
     opponent_character = melee.Character.FOX
-
 
     replay_folder = '/home/human/Documents/replays_lite/'
 
@@ -26,6 +23,7 @@ if __name__ == '__main__':
         for name in files:
             replay_paths.append(os.path.join(root, name))
 
-    replay_paths = filter_replays(replay_paths, opponent_character=opponent_character, player_character=player_character, win_only=False)
-
-
+    replay_paths = filter_replays(replay_paths, opponent_character=opponent_character,
+                                  player_character=player_character, win_only=False)
+    train(replay_paths=replay_paths, min_buffer_size=MIN_BUFFER_SIZE, player_character=player_character,
+          opponent_character=opponent_character)
