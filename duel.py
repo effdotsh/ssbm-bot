c9fc069617d3e3645cec32753238976245f9ef6a
effdotsh <ephram@eff.sh>
2022-04-12 09:55:35 -0400

    train with pytorch

--- duel.py
+++ duel.py
@@ -14,10 +14,7 @@ if __name__ == '__main__':
     character = melee.Character.FALCO
     opponent = melee.Character.FOX if not args.compete else character
 
-    if not os.path.isdir(f'{args.model_path}/{character.name}'):
-        os.makedirs(f'{args.model_path}/{character.name}')
-
-    model = keras.models.load_model("model_copy/check/")
+    model = keras.models.load_model("model/final/")
     print(model.summary())
     game = GameManager.Game(args)
     game.enterMatch(cpu_level=1, opponant_character=opponent,
@@ -30,9 +27,11 @@ if __name__ == '__main__':
         out = model.predict(np.array([inp]))[0]
         print(out)
         for i in range(len(trainer.buttons)):
-            if out[i] > 0.5:
+            if out[i] > 0.3:
                 game.controller.press_button(trainer.buttons[i][0])
-        game.controller.tilt_analog_unit(melee.Button.BUTTON_MAIN, out[-2] if abs(out[-2]) > 0.2 else 0,
-                                         out[1] if abs(out[-1]) > 0.2 else 0)
-        game.controller.tilt_analog_unit(melee.Button.BUTTON_C, out[-4] if abs(out[-4]) > 0.5 else 0,
-                                         out[-3] if abs(out[-3]) > 0.5 else 0)
+        if out[-5] > 0.3:
+            game.controller.press_shoulder(melee.Button.BUTTON_R, 1)
+        game.controller.tilt_analog_unit(melee.Button.BUTTON_MAIN, np.sign(out[-2]) if abs(out[-2]) > 0.2 else 0,
+                                         np.sign(out[1]) if abs(out[-1]) > 0.2 else 0)
+        game.controller.tilt_analog_unit(melee.Button.BUTTON_C, np.sign(out[-4]) if abs(out[-4]) > 0.5 else 0,
+                                         np.sign(out[-3]) if abs(out[-3]) > 0.5 else 0)
