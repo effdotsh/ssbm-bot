f06836e51502404c4f72f1c82bd70aea9c31076f
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2022-04-17 14:43:11 -0400

    firefoxing in observation

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from DataHandler import create_model
 args = Args.get_args()
 
 if __name__ == '__main__':
-    player_character = melee.Character.MARTH
+    player_character = melee.Character.FOX
     opponent_character = melee.Character.MARTH
     stage = melee.Stage.FINAL_DESTINATION
 
