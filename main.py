1ce5fe31781c44f9543374d27dfcf24d6ff5da9a
effdotsh <ephram@eff.sh>
2022-04-17 13:55:26 -0400

    good training

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ from DataHandler import create_model
 args = Args.get_args()
 
 if __name__ == '__main__':
-    player_character = melee.Character.CPTFALCON
-    opponent_character = melee.Character.CPTFALCON
+    player_character = melee.Character.MARTH
+    opponent_character = melee.Character.MARTH
     stage = melee.Stage.FINAL_DESTINATION
+
+    print(f'{player_character.name} vs. {opponent_character.name} on {stage.name}')
 
     f = open('replays.json', 'r')
     j = json.load(f)
 
     replay_paths = j[f'{player_character.name}_{opponent_character.name}'][stage.name]
+
     create_model(replay_paths=replay_paths, player_character=player_character,
                  opponent_character=opponent_character, stage=stage)
