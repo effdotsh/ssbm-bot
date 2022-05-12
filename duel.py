5700a5cfca771990c2ce05168d62860b8f7ab799
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2022-05-12 18:17:54 -0400

    small fixes

--- duel.py
+++ duel.py
@@ -74,14 +74,14 @@ def validate_action(action_packed, maxes, gamestate: melee, player_port: int):
 
 if __name__ == '__main__':
     character = melee.Character.FOX
-    opponent = melee.Character.FOX if not args.compete else character
+    opponent = melee.Character.MARTH if not args.compete else character
     stage = melee.Stage.FINAL_DESTINATION
     print(f'{character.name} vs. {opponent.name} on {stage.name}')
 
     tree, map = DataHandler.load_model(player_character=character, opponent_character=opponent, stage=stage)
 
     game = GameManager.Game(args)
-    game.enterMatch(cpu_level=0, opponant_character=opponent,
+    game.enterMatch(cpu_level=9, opponant_character=opponent,
                     player_character=character,
                     stage=stage, rules=False)
 
