1f16d2500f09e944d18ed0263db4ae21b424108f
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2022-06-19 17:43:15 -0400

    output array formated well

--- move_tester.py
+++ move_tester.py
@@ -9,19 +9,15 @@ import os
 import DataHandler
 import numpy as np
 
-from DataHandler import controller_states_different, generate_input
+from DataHandler import controller_states_different, generate_input, generate_output
+
 args = Args.get_args()
-
-
-
-
 
 if __name__ == '__main__':
     character = melee.Character.FOX
     opponent = melee.Character.MARTH if not args.compete else character
     stage = melee.Stage.FINAL_DESTINATION
     print(f'{character.name} vs. {opponent.name} on {stage.name}')
-
 
     game = GameManager.Game(args)
     game.enterMatch(cpu_level=0, opponant_character=opponent,
@@ -35,10 +31,12 @@ if __name__ == '__main__':
         gamestate = game.get_gamestate()
         player: melee.PlayerState = gamestate.players.get(game.controller.port)
 
-
         # if controller_states_different(player.controller_state, last_state):
         #     print(time.time())
         last_state = player.controller_state
 
-        inp = generate_input(gamestate=gamestate, player_port=game.controller.port, opponent_port=game.controller_opponent.port)
-        print(inp)
+        # inp = generate_input(gamestate=gamestate, player_port=game.controller.port, opponent_port=game.controller_opponent.port)
+        # print(inp)
+
+        out = generate_output(gamestate=gamestate, player_port=game.controller.port)
+        print(out)
