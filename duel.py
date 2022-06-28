8d746ee43eb1f61ac92eb545403b7fef47c1acef
effdotsh <ephram@eff.sh>
2022-06-27 22:12:49 -0400

    fixed self-play training

--- duel.py
+++ duel.py
@@ -20,14 +20,14 @@ from Bot import Bot
 args = Args.get_args()
 smash_last = False
 
-character = melee.Character.MARTH
-opponent = melee.Character.MARTH
+character = melee.Character.CPTFALCON
+opponent = melee.Character.CPTFALCON
 stage = melee.Stage.FINAL_DESTINATION
 level=9
 
 
 def load_model(path: str):
-    path = f'models2/{path}.pkl'
+    path = f'{path}'
     if os.path.exists(path):
         with open(path, 'rb') as file:
             data = pickle.load(file)
@@ -38,20 +38,20 @@ def load_model(path: str):
 
 
 if __name__ == '__main__':
-    file_name = f'{character.name}_v_{opponent.name}_on_{stage.name}'
+    # file_name = f'models2/{character.name}_v_{opponent.name}_on_{stage.name}.pkl'
+    file_name = 'generated_models/CPTFALCON_v_CPTFALCON_on_FINAL_DESTINATION_9_15.pkl'
     print(file_name)
 
     model: keras.Model = load_model(file_name)
-    print(model.weights[0][0])
-    # game = GameManager.Game(args)
-    # game.enterMatch(cpu_level=level, opponant_character=opponent,
-    #                 player_character=character,
-    #                 stage=stage, rules=False)
-    #
-    # bot1 = Bot(model=model, controller=game.controller, opponent_controller=game.opponent_controller)
-    # # bot2 = Bot(model=model, controller=game.opponent_controller, opponent_controller=game.controller)
-    #
-    # while True:
-    #     gamestate = game.get_gamestate()
-    #     bot1.act(gamestate)
-    #     # bot2.act(gamestate)
+    game = GameManager.Game(args)
+    game.enterMatch(cpu_level=level, opponant_character=opponent,
+                    player_character=character,
+                    stage=stage, rules=False)
+
+    bot1 = Bot(model=model, controller=game.controller, opponent_controller=game.opponent_controller)
+    # bot2 = Bot(model=model, controller=game.opponent_controller, opponent_controller=game.controller)
+
+    while True:
+        gamestate = game.get_gamestate()
+        bot1.act(gamestate)
+        # bot2.act(gamestate)
